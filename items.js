6c226fda093c527588c4417a8b995169631d1576
Jedidiah T. Rollinas <jedidiahrollinas8245@gmail.com>
2025-07-21 10:21:13 +0800

    Initial commit

---
+++ items.js
@@ -0,0 +1,24 @@
+const items = [
+  "Dungeon Keepers by Emir Karyo",
+  "Abk KD Workshops by Maryna Zevako",
+  "Maryna Zevako by Chi-Long Trieu & Chi-Binh Trieu",
+  "Biennale Souffle by Manon Bruet, Pia-Mélissa Laroche, ici l’onde—CNM",
+  "SISSYHOOD by Christine Janjira Meyer, Nino Cadeau, Emile Cerf",
+  "Office Festival by Félicité Landrivon",
+  "a little more careful by Anouk Jeanningros, Loïs Renou",
+  "DO NOT COPY HEART © by Maram Al Refaei",
+  "Nova Be||evi||e Mu|tip|es / Print by Matthieu Becker, Quintal Atelier",
+  "wubwub by Robert Radziejewski",
+  "SKINNY by Summer Yunshu Qin ircuit",
+  "Newone Awards 2024 by Tao Lin",
+  "Ankali in October 2022 by Robert Radziejewski",
+  "Leonardo’s notebook by Viter, Chou Yuwei, Puzzleman Leun",
+  "HAO All Night Long by MENSLIES",
+  "JOEL by  Yuchang Sun",
+  "DA! HEARD IT by  Hélène Marian",
+  "Lampetia of Balance – Poster 3 by  Luke Brown",
+  "Vis by  Karolina Pietrzyk, Tobias Wenig",
+  "Round Table by  Donil Lavashov",
+];
+
+export default items;
